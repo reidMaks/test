49cbcb707510e679937134dff6e77bbe91044eb9
reidMaks <reid.maks@gmail.com>
2026-06-29 14:05:06 +0300

    grafana password

--- workload/victoriametrics.tf
+++ workload/victoriametrics.tf
@@ -13,6 +13,22 @@ resource "kubernetes_namespace" "monitoring" {
   }
 }
 
+data "bitwarden-secrets_secret" "grafana_admin_password" {
+  id = "16c8b4c0-7dfb-4670-bb98-b478007a2af4"
+}
+
+resource "kubernetes_secret" "grafana" {
+  metadata {
+    name      = "grafana-secrets"
+    namespace = kubernetes_namespace.monitoring.metadata[0].name
+  }
+
+  data = {
+    admin-user     = "admin"
+    admin-password = data.bitwarden-secrets_secret.grafana_admin_password.value
+  }
+}
+
 resource "helm_release" "victoriametrics" {
   name             = "vm"
   repository       = "https://victoriametrics.github.io/helm-charts/"
@@ -23,4 +39,6 @@ resource "helm_release" "victoriametrics" {
   values = [
     file("${path.module}/helm_values/victoriametrics.yaml")
   ]
+
+  depends_on = [kubernetes_secret.grafana]
 }
