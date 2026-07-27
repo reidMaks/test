bfdec90f8df5ef8cda2aba9ca8f863e3eab61d88
reidMaks <reid.maks@gmail.com>
2026-07-27 15:33:16 +0300

    Add Traefik dynamic config and replace symlinks for Portainer

--- infra/shared_bitwarden_provider.tf
+++ infra/shared_bitwarden_provider.tf
@@ -1,1 +1,6 @@
-../shared_bitwarden_provider.tf
+provider "bitwarden-secrets" {
+  api_url      = "https://api.bitwarden.eu"
+  identity_url = "https://identity.bitwarden.eu"
+
+  organization_id = "dd4d4a61-6bc9-4fce-9f6d-b41b0077db8c"
+}
